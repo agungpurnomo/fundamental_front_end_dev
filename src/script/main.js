6fbb7032adc7de266497e1ed6a900f4c285f89d3
agungpurnomo <agung.laravel@gmail.com>
2020-04-13 16:52:27 +0700

    progress

--- src/script/main.js
+++ src/script/main.js
@@ -1,5 +1,5 @@
 function main(){
-	window.addEventListener("scroll", function(){
+	window.addEventListener("scroll",()=>{
 	let navArea = document.getElementById('nav-area');
 
 		if(window.pageYOffset>0){
@@ -12,7 +12,7 @@ function main(){
 	});
 
 
-	$(document).ready(function(){
+	$(document).ready(()=>{
 		let ajaxResponse;
 		$(".button").click(function(){
 			$("#main").html('');
@@ -24,7 +24,7 @@ function main(){
 				data: {
 					'api_key': 'a86239bbc54069065ee3fa0be9e43e65'
 				},
-				success: function(list) {
+				success: (list)=> {
 					if (list.Response = "True") {
 						setTimeout(() => {
 							$(".button").removeClass("active");
@@ -36,17 +36,17 @@ function main(){
 							<div id="listMovie" class="row justify-content-center mt-4">
 								<div class="loader"></div>
 							</div>`);
-							$.each(ajaxResponse, function(i,item){
+							$.each(ajaxResponse, (i,item)=>{
 								$('#listMovie').append(`	
 									<div class="md-col-3 mb-4" style="padding-right: 10px;">
 										<div class="card" style="width: 14rem;">
 											<img src="https://image.tmdb.org/t/p/w185_and_h278_bestv2/` + item.poster_path + `" class="card-img-top" alt="...">
 											<div class="card-body">
 												<h5 class="card-title">`+item.title+`</h5>
-												<p class="card-text" style="margin-bottom:0;">`+ $.datepicker.formatDate("yy", new Date(item.release_date)) +`</p>
+												<p class="card-text" style="margin-bottom:0;"></p>
 												<p class="card-text">Rating : `+ item.vote_average +`</p>
-												<a href="#" type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" style="border-radius: 1.25rem;
-												width: 100%;" >Overview</a>
+												<!--<a href="#" type="button" class="btn btn-primary"  style="border-radius: 1.25rem;
+												width: 100%;" >Overview</a>-->
 											</div>
 										</div>
 									</div>`
@@ -70,7 +70,7 @@ function main(){
 						'api_key': 'a86239bbc54069065ee3fa0be9e43e65',
 						'query' : $('#search').val()
 					},
-					success: function(list) {
+					success: (list)=> {
 						let searchMovie;
 						if (list.Response = "True") {
 							searchMovie=list.results;
@@ -82,18 +82,18 @@ function main(){
 								<div class="loader"></div>
 							</div>`);
 
-							$.each(searchMovie, function(i,item){
+							$.each(searchMovie, (i,item)=>{
 								$('#listMovie').append(`	
 								<div class="md-col-3 mb-4" style="padding-right: 10px;">
 								<div class="card" style="width: 14rem;">
 									  <img src="https://image.tmdb.org/t/p/w185_and_h278_bestv2/` + item.poster_path + `" class="card-img-top" alt="...">
 									  <div class="card-body">
 										<h5 class="card-title">`+item.title+`</h5>
-										<p class="card-text">Release date :`+ $.datepicker.formatDate("D dd-M-yy", new Date(item.release_date)) +`</p>
+										<p class="card-text">Release date :</p>
 										<p class="card-text">Rating :`+ item.vote_average +`</p>
-										<a href="#" type="button" 
-										class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" style="border-radius: 1.25rem;
-										width: 100%;">Overview</a>
+										<!--<a href="#" type="button" 
+										class="btn btn-primary"  style="border-radius: 1.25rem;
+										width: 100%;">Overview</a>-->
 									  </div>
 								</div>`);
 							});
